84891cc27b124b96210f7b4fc75bd0e6c51d2e8c
Manea Loredana <76653089+Loredana-Manea-B1@users.noreply.github.com>
2022-02-03 10:45:52 +0200

    Update Autor.scala

--- Sesiune/Autor.scala
+++ Sesiune/Autor.scala
@@ -5,15 +5,66 @@ import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
 import com.cra.figaro.library.compound.CPD
 
+import com.cra.figaro.algorithm.OneTimeMPE
+import com.cra.figaro.algorithm.factored.MPEVariableElimination
+import com.cra.figaro.algorithm.factored.beliefpropagation.MPEBeliefPropagation
+import com.cra.figaro.algorithm.sampling.{MetropolisHastingsAnnealer, Schedule, _}
+import com.cra.figaro.language.Universe.universe
+import com.cra.figaro.language.{Flip, _}
+import com.cra.figaro.library.compound.If
 
-	class Album {
-		val liked = Uniform(0, 0.1)
+	object examen{
 
-	class Autor(album: List[Album]) {
+		abstract class Autor() {
 		private val likedContainer = Container(album.map(_.liked):_×)
 		private val popularityMean = likedContainer.foldLeft(0.0)(_ max _)
 		val popularity = Normal(popularityMean, 5)
+			val confident: Element[Boolean]
+			def possession: Element[Boolean] =
+				If(confident, Flip(0.1), Flip(0.5))
 	}
+
+		class InitialAutor() extends Autor {
+			val confident = Flip(0.4)
+		}
+
+		class NextAutor(current: State) extends Autor {
+			val confident = If(current.confident, Flip(0.6), Flip(0.3))
+		}
+		// produce a state sequence in reverse order of the given length
+
+		def AutorSequence(n: Int): List[Autor] = {
+			if (n == 0)
+				List(new InitialAutor())
+			else {
+				val last :: rest = autorSequence(n - 1)//add la inceputul listei un element ::
+				new  NextAutor(last) :: last :: rest
+			}
+		}
+
+
+		def run(algorithm: OneTimeMPE) {
+			val obsSeq: List[Boolean] = List.fill(10)(scala.util.Random.nextBoolean())
+			val stateSeq = autorSequence(obsSeq.length)
+
+
+			for {i <- obsSeq.indices} {
+				//facem observatiile
+				stateSeq(i).possession.observe(obsSeq(obsSeq.length - 1 - i))
+				print("\t")
+			}
+
+			//pornim algoritmul , DUPA ce am facut observatiile
+			algorithm.start()
+
+
+		}
+
+
+		class Album {
+			val liked = Uniform(0, 0.1)
+		}
+
 
 	val album = Array.fill(10)(new Autor)
 	val melodia1 = new Melodia(List(autor(0), autor(1)))
@@ -32,9 +83,7 @@ import com.cra.figaro.library.compound.CPD
 
 	class Nominalizare(album: Album){
 
-		def getProb(): Unit ={
 
-		}
 
 
 	}
